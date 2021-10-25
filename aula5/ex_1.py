7d5fde4a8d621a6c81f0ab653fde534f9733e52e
Marcelo Nincao <mnincao@gmail.com>
2021-10-25 20:25:02 -0300

    Adicionando arquivos da aula 5

---
+++ aula5/ex_1.py
@@ -0,0 +1,17 @@
+# 1) Escreva um programa em python que conte as vogais que a música ‘Faroeste Caboclo’
+# tem em sua letra. Armazena a letra da música em um arquivo do tipo txt.
+# Dica: Não se esqueça de considerar as letras maiúsculas, minúsculas e com acentuação.
+
+def contar_vogais():
+    vogais = {
+        'a': 0,
+        ...
+    }
+
+    ...
+
+    return vogais
+
+def main():
+    pass
+
